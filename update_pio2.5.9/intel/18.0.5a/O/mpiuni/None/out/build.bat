f9d6bf7cf9c9f4f4fc8b8da5840506a30960417b
Gerhard Theurich <theurich@sourcespring.net>
2022-09-30 02:57:52 +0000

    action=collect dir=gfortran_9.2.0b_openmpi_O_develop branch=develop hash=v8.4.0b15-7-gae8efd6 phase=test

--- update_pio2.5.9/intel/18.0.5a/O/mpiuni/None/out/build.bat
+++ update_pio2.5.9/intel/18.0.5a/O/mpiuni/None/out/build.bat
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 #SBATCH --account=nems
-#SBATCH -o /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/build.bat_%j.o
-#SBATCH -e /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/build.bat_%j.e
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/build.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/build.bat_%j.e
 #SBATCH --time=1:00:00
 #SBATCH --partition=hera
 #SBATCH --qos=batch
@@ -16,16 +16,16 @@ module load intel/18.0.5.274
 module load netcdf/4.6.1
 
 set -x
-export ESMF_DIR=/scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/esmf
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/esmf
 export ESMF_COMPILER=intel
 export ESMF_COMM=mpiuni
 export ESMF_NETCDF=nc-config
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-export ESMF_MPIRUN=/scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/esmf/src/Infrastructure/stubs/mpiuni/mpirun
-module list >& /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/module-build.log
-cd /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/esmf
+export ESMF_MPIRUN=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/module-build.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5a_mpiuni_O_update_pio2.5.9/esmf
 set -o pipefail
 make info 2>&1| tee ../info.log
 make -j 40 2>&1| tee ../build.log
