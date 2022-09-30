2bca68773ae1d1f88d0ed042a6c613474853f0ab
Gerhard Theurich <theurich@sourcespring.net>
2022-09-30 02:39:59 +0000

    action=clear dir=intel_18.0.5a_mpiuni_g_update_pio2.5.9 branch=update_pio2.5.9 hash=v8.4.0b15-9-g5b6dda3 phase=build

--- develop/gfortran/9.2.0b/g/openmpi/3.1.4/out/build.bat
+++ develop/gfortran/9.2.0b/g/openmpi/3.1.4/out/build.bat
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 #SBATCH --account=nems
-#SBATCH -o /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/gfortran_9.2.0b_openmpi_g_develop/build.bat_%j.o
-#SBATCH -e /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/gfortran_9.2.0b_openmpi_g_develop/build.bat_%j.e
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_g_develop/build.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_g_develop/build.bat_%j.e
 #SBATCH --time=1:00:00
 #SBATCH --partition=hera
 #SBATCH --qos=batch
@@ -16,14 +16,14 @@ export ESMF_MPIRUN=mpirun.srun
 module load gnu/9.2.0 openmpi/3.1.4
 
 set -x
-export ESMF_DIR=/scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/gfortran_9.2.0b_openmpi_g_develop/esmf
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_g_develop/esmf
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
 export ESMF_BOPT='g'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-module list >& /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/gfortran_9.2.0b_openmpi_g_develop/module-build.log
-cd /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/gfortran_9.2.0b_openmpi_g_develop/esmf
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_g_develop/module-build.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/gfortran_9.2.0b_openmpi_g_develop/esmf
 set -o pipefail
 make info 2>&1| tee ../info.log
 make -j 40 2>&1| tee ../build.log
--- develop/intel/18.0.5b/O/intelmpi/2018.4.274/out/test.bat
+++ develop/intel/18.0.5b/O/intelmpi/2018.4.274/out/test.bat
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 #SBATCH --account=nems
-#SBATCH -o /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5b_intelmpi_O_develop/test.bat_%j.o
-#SBATCH -e /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5b_intelmpi_O_develop/test.bat_%j.e
+#SBATCH -o /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5b_intelmpi_O_develop/test.bat_%j.o
+#SBATCH -e /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5b_intelmpi_O_develop/test.bat_%j.e
 #SBATCH --time=2:00:00
 #SBATCH --partition=hera
 #SBATCH --qos=batch
@@ -17,15 +17,15 @@ module load intel/18.0.5.274 impi/2018.4.274
 module load netcdf-hdf5parallel/4.7.4
 
 set -x
-export ESMF_DIR=/scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5b_intelmpi_O_develop/esmf
+export ESMF_DIR=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5b_intelmpi_O_develop/esmf
 export ESMF_COMPILER=intel
 export ESMF_COMM=intelmpi
 export ESMF_NETCDF=nc-config
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-module list >& /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5b_intelmpi_O_develop/module-test.log
-cd /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/intel_18.0.5b_intelmpi_O_develop/esmf
+module list >& /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5b_intelmpi_O_develop/module-test.log
+cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/intel_18.0.5b_intelmpi_O_develop/esmf
 make install 2>&1| tee ../install.log
 make all_tests 2>&1| tee ../test.log
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
