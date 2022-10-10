4a77cd2affc96eac73293bd6879d318ad469181f
Gerhard Theurich <theurich@sourcespring.net>
2022-10-10 11:42:25 -0600

    action=collect dir=gfortran_11.2.0_mpiuni_O_develop branch=develop hash=v8.4.0b18-4-g95e3a6c phase=build

--- develop/gfortran/11.2.0/O/mpiuni/None/out/build.bat
+++ develop/gfortran/11.2.0/O/mpiuni/None/out/build.bat
@@ -5,16 +5,16 @@ module load gcc/11.2.0
 module load netcdf/4.7.4
 
 set -x
-export ESMF_DIR=/project/esmf/rocky/esmf-testing/gfortran_11.2.0_mpiuni_O_develop/esmf
+export ESMF_DIR=/project/esmf/theurich/ESMF-Nightly-Testing/gfortran_11.2.0_mpiuni_O_develop/esmf
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpiuni
 export ESMF_NETCDF=nc-config
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-export ESMF_MPIRUN=/project/esmf/rocky/esmf-testing/gfortran_11.2.0_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
-module list >& /project/esmf/rocky/esmf-testing/gfortran_11.2.0_mpiuni_O_develop/module-build.log
-cd /project/esmf/rocky/esmf-testing/gfortran_11.2.0_mpiuni_O_develop/esmf
+export ESMF_MPIRUN=/project/esmf/theurich/ESMF-Nightly-Testing/gfortran_11.2.0_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /project/esmf/theurich/ESMF-Nightly-Testing/gfortran_11.2.0_mpiuni_O_develop/module-build.log
+cd /project/esmf/theurich/ESMF-Nightly-Testing/gfortran_11.2.0_mpiuni_O_develop/esmf
 set -o pipefail
 make info 2>&1| tee ../info.log
 make -j 4 2>&1| tee ../build.log
